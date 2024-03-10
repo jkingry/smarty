54c16e4b467a5f6c9e458facb28af1f80b4636a9
Joe Kingry <joe@kingry.ca>
2024-03-09 19:55:57 -0500

    typing and docs

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
+"""Smart Calculator - Count smarty eating time for an input file.
+
+Usage:
+    python main.py <input_file>
+
+Example:
+    python main.py input.txt
+"""
+
 import sys
 import io
 
 from smarty_counters import SmartyCounter, SmartyColourCalculator
 
 
-def run(input_file):
+def run(input_file: str):
+    """
+    Run the smart calculator with the input file.
+    """
     calc = SmartyColourCalculator(default_size=7, default_time=7)
     calc.group_size["red"] = 1
     calc.group_time["red"] = 13
 
     counter = SmartyCounter(calc)
 
-    input_source = io.open(input_file, "r", encoding="utf-8")
-
-    for raw_line in input_source:
-        line = raw_line.strip()
-
-        counter.add(line)
-
-    counter.finish()
-
-    print(counter.elapsed_seconds)
+    with io.open(input_file, "r", encoding="utf-8") as input_source:
+        for raw_line in input_source:
+            line = raw_line.strip()
+            counter.add(line)
+        counter.finish()
+        print(counter.elapsed_seconds)
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(__doc__)
+        sys.exit(1)
+
     run(sys.argv[1])
--- smarty_counters.py
+++ smarty_counters.py
@@ -1,21 +1,51 @@
+"""Smarty counters module"""
+
 from collections import defaultdict
+from typing import Dict
 
 
 class SmartyColourCalculator:
-    BOX_LINE = "box"
+    """
+    A class that calculates the finish time for a set of Smarty colours.
+
+    Attributes:
+        BOX_LINE (str): A constant representing the line that indicates the end of a box.
+
+    Methods:
+        __init__(self, group_size=None, group_time=None, default_size=7, default_time=7):
+            Initializes a new instance of the SmartyColourCalculator class.
+        finish_box(self):
+            Calculates and returns the finish time for the current box.
+        reset(self):
+            Resets the group counts to zero.
+        add_smarty(self, color):
+            Adds a Smarty of the specified color to the group counts and returns the time 
+            it takes to complete a group.
+        add(self, line):
+            Adds a line to the SmartyColourCalculator instance and returns the finish time 
+            if it's the end of a box, otherwise returns the time it takes to complete a group.
+    """
+
+    BOX_LINE: str = "box"
 
     def __init__(
         self,
-        group_size=None,
-        group_time=None,
-        default_size=7,
-        default_time=7
+        group_size: Dict[str, int] = None,
+        group_time: Dict[str, int] = None,
+        default_size: int = 7,
+        default_time: int = 7,
     ):
-        self.group_size = defaultdict(lambda: default_size, **group_size or {})
-        self.group_time = defaultdict(lambda: default_time, **group_time or {})
+        self.group_size: Dict[str, int] = defaultdict(lambda: default_size, **group_size or {})
+        self.group_time: Dict[str, int]  = defaultdict(lambda: default_time, **group_time or {})
         self.group_counts = defaultdict(int)
 
-    def finish_box(self):
+    def finish_box(self) -> int:
+        """
+        Calculates the total finish time for all groups in the box.
+
+        Returns:
+            The total remaining time for all partial groups
+        """
         finish_time = 0
         for color in self.group_counts:
             finish_time += self.group_time[color] * self.group_counts[color]
@@ -23,10 +53,17 @@ class SmartyColourCalculator:
 
         return finish_time
 
-    def reset(self):
+    def reset(self) -> None:
+        """
+        Resets the group counts to zero.
+        """
         self.group_counts.clear()
 
-    def add_smarty(self, color):
+    def add_smarty(self, color: str) -> int:
+        """
+        Adds a Smarty of the specified color to the group counts and
+        returns the time it takes to complete a group .
+        """
         self.group_counts[color] += 1
 
         if self.group_counts[color] == self.group_size[color]:
@@ -35,7 +72,11 @@ class SmartyColourCalculator:
 
         return 0
 
-    def add(self, line):
+    def add(self, line: str) -> int:
+        """
+        Adds a line to the SmartyColourCalculator instance and returns the finish time
+        if it's the end of a box, otherwise returns the time it takes to complete a group.
+        """
         if line == SmartyColourCalculator.BOX_LINE:
             return self.finish_box()
         else:
@@ -43,16 +84,41 @@ class SmartyColourCalculator:
 
 
 class SmartyCounter:
+    """
+    A class representing a Smarty Counter.
+
+    Attributes:
+        calculator (SmartyColourCalculator): The calculator used for calculating elapsed time.
+        elapsed_seconds (int): The total elapsed seconds.
+
+    Methods:
+        finish(): Finishes the current box and adds the elapsed time to the total.
+        add(line: str): Adds the elapsed time for a line to the total.
+        reset(): Resets the elapsed time and the calculator.
+    """
+
     def __init__(self, calculator: SmartyColourCalculator):
         self.calculator = calculator
         self.elapsed_seconds = 0
 
-    def finish(self):
+    def finish(self) -> None:
+        """
+        Finishes the current box and adds the elapsed time to the total.
+        """
         self.elapsed_seconds += self.calculator.finish_box()
 
-    def add(self, line):
+    def add(self, line: str) -> None:
+        """
+        Adds the elapsed time for a line to the total.
+
+        Args:
+            line (str): The line to calculate the elapsed time for.
+        """
         self.elapsed_seconds += self.calculator.add(line)
 
-    def reset(self):
+    def reset(self) -> None:
+        """
+        Resets the elapsed time and the calculator.
+        """
         self.elapsed_seconds = 0
         self.calculator.reset()
